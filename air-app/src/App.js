fc93f5fc808598ce781222e8759ad5fc46fc5e69
MLScholl <mls2scholl@gmail.com>
2018-10-02 16:23:41 -0500

    Update

--- air-app/src/App.js
+++ air-app/src/App.js
@@ -4,54 +4,68 @@ import 'react-table/react-table.css';
 import { getInitialFlightData } from './DataProvider';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: getInitialFlightData()
-    };
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: getInitialFlightData()
+		};
 
-    this.columns = [{
-      Header: 'Origin',
-      accessor: 'origin'
-    }, {
-      Header: 'Flight',
-      accessor: 'flight'
-    }, {
-      Header: 'Arrival',
-      accessor: 'arrival'
-    }, {
-      Header: 'State',
-      accessor: 'state'
-    }];
+		this.columns = [{
+			Header: 'Origin',
+			accessor: 'origin'
+		}, {
+			Header: 'Flight',
+			accessor: 'flight'
+		}, {
+			Header: 'Arrival',
+			accessor: 'arrival'
+		}, {
+			Header: 'State',
+			accessor: 'state'
+		}];
 
-    this.eventSource = new EventSource('http://localhost:5000/events');
-    
+		this.eventSource = new EventSource('http://localhost:5000/events');
+
+	}
+
+	componentDidMount() {
+		this.eventSource.addEventListener('flightStateUpdate', (e) => this.updateFlightState(JSON.parse(e.data)));
+		this.eventSource.addEventListener('flightRemoval', (e) => this.removeFlight(JSON.parse(e.data)));
+		this.eventSource.addEventListener('closedConnection', () => this.stopUpdates());
+	}
+
+	updateFlightState(flightState) {
+		let newData = this.state.data.map((item) => {
+			if (item.flight === flightState.flight) {
+				item.state = flightState.state;
+			}
+				return item;
+		});
+
+		this.setState(Object.assign({}, {data: newData}));
+	}
+
+	removeFlight(flightInfo) {
+    const newData = this.state.data.filter((item) => item.flight !== flightInfo.flight);
+
+    this.setState(Object.assign({}, {data: newData}));
   }
 
-  componentDidMount() {
-      this.eventSource.onmessage = (e) => this.updateFlightState(JSON.parse(e.data));
-    }
+	stopUpdates() {
+  	this.eventSource.close();
+	}
 
-    updateFlightState(flightState) {
-      let newData = this.state.data.map((item) => {
-        if (item.flight === flightState.flight) {
-          item.state = flightState.state;
-        }
-        return item;
-      });
-
-      this.setState(Object.assign({}, {data: newData}));
-    }
-  render() {
-    return (
-      <div className="App">
-        <ReactTable
-          data={this.state.data}
-          columns={this.columns}
-        />
-      </div>
-    );
-  }
+	render() {
+  	return (
+    	<div className="App">
+      	<button onClick={() => this.stopUpdates()}>Stop updates</button>
+      	<ReactTable
+        	data={this.state.data}
+        	columns={this.columns}
+      	/>
+    	</div>
+  	);
+	}
 }
 
 export default App;
--- air-backend/server.js
+++ air-backend/server.js
@@ -3,6 +3,13 @@ const http = require('http');
 http.createServer((request, response) => {
   console.log('Requested url: ' + request.url);
 
+  request.on('close', () => {
+    if (!response.finished) {
+      response.end();
+      console.log('Stopped sending events.');
+    }
+  });
+  
   if (request.url.toLowerCase() === '/events') {
     response.writeHead(200, {
       'Connection': 'keep-alive',
@@ -13,6 +20,9 @@ http.createServer((request, response) => {
 
     setTimeout(() => {
       response.write(
+        'event: flightStateUpdate\n'
+      );
+      response.write(
         'data: {"flight": "I768", "state": "landing"}'
       );
       response.write('\n\n');
@@ -20,10 +30,23 @@ http.createServer((request, response) => {
 
     setTimeout(() => {
       response.write(
+        'event: flightStateUpdate\n'
+      );
+      response.write(
         'data: {"flight": "I768", "state": "landed"}'
       );
       response.write('\n\n');
     }, 6000);
+
+    setTimeout(() => {
+      response.write(
+        'event: flightRemoval\n'
+      );
+      response.write(
+        'data: {"flight": "I768"}'
+      );
+      response.write('\n\n');
+    }, 9000);
 
   } else {
     response.writeHead(404);
